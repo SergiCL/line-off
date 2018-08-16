555658382d5ed2b3da3d9dfcf41070a35e106dd0
SergiCL <s.cuen.luq@gmail.com>
2018-08-16 20:23:53 +0200

    Dead by collision

--- src/js/game.js
+++ src/js/game.js
@@ -23,17 +23,45 @@ var player = kontra.sprite({
         else if (this.y < (canvas.height - this.height) && kontra.keys.pressed('down')) {
             this.y += 1
         }
+
+        if(this.collidesWith(enemy)) {
+            alert("You lose!");
+            resetGame();
+        }
     }
 });
 
+var enemy = kontra.sprite({
+    x: 150,
+    y: 40,
+    color: 'orange',
+    radius: 4,
+
+    render: function() {
+        this.context.fillStyle = this.color;
+
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.context.fill();
+    }
+});
+
+function resetGame() {
+    player.x = 140;
+    player.y = 65;
+    enemy.x = 150;
+    enemy.y = 40;
+}
 
 var loop  = kontra.gameLoop({
     update: function () {
         player.update();
+        enemy.update();
         console.log(player.x+" - "+player.y);
     },
     render: function () {
         player.render();
+        enemy.render();
     }
 });
 
